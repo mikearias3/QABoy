10c5f5d3605174ed04b2242ccb95e4ec60e9adb9
Mike Arias <michael.arias.93@gmail.com>
2018-03-01 19:42:46 +0100

    no message

--- QABoyAutomation-Final/QABoyAutomationFramework/webdriver.py
+++ QABoyAutomation-Final/QABoyAutomationFramework/webdriver.py
@@ -4,7 +4,17 @@ from selenium import webdriver
 class Driver:
 
     def __init__(self):
-        self.instance = webdriver.Chrome()
+        desired_cap = {
+            'browser': 'Chrome',
+            'browser_version': '64.0',
+            'os': 'Windows',
+            'os_version': '10',
+            'resolution': '1024x768'
+        }
+
+        self.instance = webdriver.Remote(
+            command_executor='http://mikearias2:6FB489KGkKpshxcFyqzR@hub.browserstack.com:80/wd/hub',
+            desired_capabilities=desired_cap)
 
     def navigate(self, url):
         if isinstance(url, str):
